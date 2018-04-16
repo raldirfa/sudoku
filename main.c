0b48698f11e1eb1bbe005552062819645857017b
Raul Aldir Fabbro <r.aldirfabbro@gmail.com>
2018-04-16 20:25:02 +0200

    Add sollution level for debugging

--- main.c
+++ main.c
@@ -41,63 +41,33 @@ int new_game()
 {
     system("cls");
     int won = 0;
-    SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE];
-    fill_sudoku(arraySudoku);
-        //Spiel 1
-    {
-        arraySudoku[0][0].value = 7;
-        arraySudoku[0][1].value = 9;
-        arraySudoku[0][4].value = 5;
-        arraySudoku[0][5].value = 8;
-        arraySudoku[0][6].value = 2;
+    //  SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE];
+    //  fill_sudoku(arraySudoku);
 
-        arraySudoku[1][2].value = 4;
-        arraySudoku[1][3].value = 6;
-        arraySudoku[1][5].value = 7;
-        arraySudoku[1][7].value = 5;
-        arraySudoku[1][8].value = 8;
+    /*
+    // Level 1
+    SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE] = {   {{7},{9},{0},{0},{5},{8},{2},{0},{0}},
+                                                            {{0},{0},{4},{6},{0},{7},{0},{5},{8}},
+                                                            {{5},{0},{3},{0},{0},{2},{6},{7},{0}},
+                                                            {{0},{4},{0},{2},{7},{0},{5},{0},{6}},
+                                                            {{0},{3},{9},{5},{0},{0},{1},{8},{0}},
+                                                            {{6},{7},{0},{0},{1},{9},{0},{0},{2}},
+                                                            {{9},{0},{0},{7},{0},{1},{0},{0},{4}},
+                                                            {{0},{6},{8},{0},{0},{5},{7},{0},{0}},
+                                                            {{3},{0},{7},{4},{8},{0},{0},{2},{5}}};
+    /*
+    //Level 1 Solution
+    SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE] = {   {{7},{9},{6},{3},{5},{8},{2},{4},{1}},
+                                                            {{1},{2},{4},{6},{9},{7},{3},{5},{8}},
+                                                            {{5},{8},{3},{1},{4},{2},{6},{7},{9}},
+                                                            {{8},{4},{1},{2},{7},{3},{5},{9},{6}},
+                                                            {{2},{3},{9},{5},{6},{4},{1},{8},{7}},
+                                                            {{6},{7},{5},{8},{1},{9},{4},{3},{2}},
+                                                            {{9},{5},{2},{7},{3},{1},{8},{6},{4}},
+                                                            {{4},{6},{8},{9},{2},{5},{7},{1},{3}},
+                                                            {{3},{1},{7},{4},{8},{6},{9},{2},{5}}};
+    */
 
-        arraySudoku[2][0].value = 5;
-        arraySudoku[2][2].value = 3;
-        arraySudoku[2][5].value = 2;
-        arraySudoku[2][6].value = 6;
-        arraySudoku[2][7].value = 7;
-
-        arraySudoku[3][1].value = 4;
-        arraySudoku[3][3].value = 2;
-        arraySudoku[3][4].value = 7;
-        arraySudoku[3][6].value = 5;
-        arraySudoku[3][8].value = 6;
-
-        arraySudoku[4][1].value = 3;
-        arraySudoku[4][2].value = 9;
-        arraySudoku[4][3].value = 5;
-        arraySudoku[4][6].value = 1;
-        arraySudoku[4][7].value = 8;
-
-        arraySudoku[5][0].value = 6;
-        arraySudoku[5][1].value = 7;
-        arraySudoku[5][4].value = 1;
-        arraySudoku[5][5].value = 9;
-        arraySudoku[5][8].value = 2;
-
-        arraySudoku[6][0].value = 9;
-        arraySudoku[6][3].value = 7;
-        arraySudoku[6][5].value = 1;
-        arraySudoku[6][8].value = 4;
-
-        arraySudoku[7][1].value = 6;
-        arraySudoku[7][2].value = 8;
-        arraySudoku[7][5].value = 5;
-        arraySudoku[7][6].value = 7;
-
-        arraySudoku[8][0].value = 3;
-        arraySudoku[8][2].value = 7;
-        arraySudoku[8][3].value = 4;
-        arraySudoku[8][4].value = 8;
-        arraySudoku[8][7].value = 2;
-        arraySudoku[8][8].value = 5;
-    }
     set_editable(arraySudoku);
 
     int row = 0;
