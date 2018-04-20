73e62227a13bfa89286196b37bf3183cbf7afd5e
Raul Aldir Fabbro <r.aldirfabbro@gmail.com>
2018-04-20 21:52:07 +0200

    Add feature scalable

--- sudoku.c
+++ sudoku.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include "sudoku.h"
 
+void print_seperator()
+{
+    int i, j;
+    for (i = 0; i < SUDOKU_SEPERATOR; i++)
+    {
+        printf("+");
+        for (j = 0; j < SUDOKU_SEPERATOR * 3; j++)
+        {
+            printf("-");
+        }
+    }
+    printf("+\n");
+}
+
 void fill_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE])
 {
     int i, j;
@@ -20,18 +34,25 @@ void print_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE])
     int i, j;
     for (i = 0; i < SUDOKU_SIZE; i++)
     {
-        if(i%3 == 0)
+        if(i%SUDOKU_SEPERATOR == 0)
         {
-            printf("+---------+---------+---------+\n");
+            print_seperator();
         }
         for (j = 0; j < SUDOKU_SIZE; j++)
         {
-            if(j%3 == 0)
+            if(j%SUDOKU_SEPERATOR == 0)
             {
                 printf("|");
             }
             if(arraySudoku[i][j].value != 0){
-                printf(" %d ", arraySudoku[i][j].value);
+                if (arraySudoku[i][j].value > 9)
+                {
+                    printf("%d ", arraySudoku[i][j].value);
+                }
+                else
+                {
+                    printf(" %d ", arraySudoku[i][j].value);
+                }
             } else{
                 printf("   ");
             }
@@ -40,7 +61,7 @@ void print_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE])
 
         printf("|\n");
     }
-    printf("+---------+---------+---------+\n");
+    print_seperator();
     printf("\nInput:\n"
            "[$column $row $value]\n"
            "[h] for help\n"
@@ -56,13 +77,28 @@ void print_error(char error[]){
     printf("\n");
 }
 
+int fill_check_array(int array[SUDOKU_SIZE])
+{
+    int i;
+    for (i = 0; i < SUDOKU_SIZE; i++)
+    {
+        array[i] = 0;
+    }
+    return 0;
+}
+
 int check_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE]){
 
     int i, j, k, l, x;
-    static int initCheck[9] = {0,0,0,0,0,0,0,0,0};
-    int checkRow[9] = {0,0,0,0,0,0,0,0,0};
-    int checkCol[9] = {0,0,0,0,0,0,0,0,0};
-    int checkBox[9] = {0,0,0,0,0,0,0,0,0};
+    static int initCheck[SUDOKU_SIZE];
+    int checkRow[SUDOKU_SIZE];
+    int checkCol[SUDOKU_SIZE];
+    int checkBox[SUDOKU_SIZE];
+
+    fill_check_array(initCheck);
+    fill_check_array(checkRow);
+    fill_check_array(checkCol);
+    fill_check_array(checkBox);
 
     //checkRows:
     {
@@ -86,7 +122,7 @@ int check_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE]){
     //checkCols:
     {
         for(i = 0; i < SUDOKU_SIZE; i++){
-                memmove(checkCol,initCheck,sizeof(checkCol));
+            memmove(checkCol,initCheck,sizeof(checkCol));
             for(j = 0; j < SUDOKU_SIZE; j++){
                 x = arraySudoku[j][i].value;
                 if(x == 0){
@@ -106,11 +142,11 @@ int check_sudoku(SudokuField arraySudoku[SUDOKU_SIZE][SUDOKU_SIZE]){
     {
         //i and j point to the first field in every 3x3 box
         //k and l iterate through every field in the box
-        for(i = 0; i < SUDOKU_SIZE; i = i + 3){
-            for(j = 0; j < SUDOKU_SIZE; j = j + 3){
+        for(i = 0; i < SUDOKU_SIZE; i = i + SUDOKU_SEPERATOR){
+            for(j = 0; j < SUDOKU_SIZE; j = j + SUDOKU_SEPERATOR){
                 memmove(checkBox,initCheck,sizeof(checkBox));
-                for(k = 0; k < 3; k++){
-                    for(l = 0; l < 3 ; l++){
+                for(k = 0; k < SUDOKU_SEPERATOR; k++){
+                    for(l = 0; l < SUDOKU_SEPERATOR; l++){
                         x = arraySudoku[i+k][j+l].value;
                         if(x == 0){
                             return 0;
